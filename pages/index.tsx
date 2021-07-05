8e0909aebf25f8c3891c6388bc722f1b74e262d3
Anthony Lee <antwonlee@gmail.com>
2021-07-05 15:52:21 -0500

    Install google fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,12 +11,18 @@ export default function Home() {
           name="description"
           content="Bringing healthcare billing power back to patience"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap"
+          rel="stylesheet"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>DaalBeat</h1>
-        <p className="text-xl text-gray-500">
+        <h1 className="font-work text-5xl font-extrabold">DaalBeat</h1>
+        <p className="font-work text-lg text-gray-500">
           Bringing healthcare billing power back to patience.
         </p>
       </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        work: ["Work Sans", "sans-serif"],
+      },
+    },
   },
   variants: {
     extend: {},
