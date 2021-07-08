61d62875181597eef90363d667bc3ba893c6ce1e
Anthony Lee <antwonlee@gmail.com>
2021-07-08 16:43:08 -0500

    feat: Add nextauth (#2)
    * Add nextauth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import Layout from "../components/Layout";
+import { Provider } from "next-auth/client";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />;
-    </Layout>
+    <Provider session={pageProps.session}>
+      <Layout>
+        <Component {...pageProps} />;
+      </Layout>
+    </Provider>
   );
 }
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 export default function Home() {
+  const [session] = useSession();
+
   return (
     <div className={styles.containerDoor}>
       <main className={styles.main}>
@@ -12,6 +15,18 @@ export default function Home() {
         <p className="font-work text-lg text-gray-500 text-center">
           Healthcare pricing transparent and simplified.
         </p>
+        <div className="mt-10 text-center">
+          {!session && (
+            <>
+              <button onClick={() => signIn()}>Sign In</button>
+            </>
+          )}
+          {session && (
+            <>
+              <button onClick={() => signOut()}>Sign Out</button>
+            </>
+          )}
+        </div>
       </main>
     </div>
   );
