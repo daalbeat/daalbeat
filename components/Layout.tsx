453a82fdfb0b3108e69e177491ae6513702baa92
Anthony Lee <antwonlee@gmail.com>
2021-07-08 14:54:42 -0500

    Update meta description

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,7 +7,7 @@ const Layout = ({ children }: any) => {
         <title>DaalBeat</title>
         <meta
           name="description"
-          content="Easy access to complicated healthcare pricing."
+          content="Giving easy access to complicated healthcare pricing."
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap"
