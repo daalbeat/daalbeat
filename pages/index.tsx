d9db4dcb5e7e2b43732493b4fae5327ef8141d0a
Anthony Lee <antwonlee@gmail.com>
2021-07-08 13:10:41 -0500

    Add prisma settings
    - Set local and production postgres and prisma

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,23 +4,12 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>DaalBeat</title>
-        <meta
-          name="description"
-          content="Healthcare Pricing Transparency made easy."
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+    <div className={styles.containerDoor}>
       <main className={styles.main}>
-        <h1 className="font-work text-5xl font-extrabold">DaalBeat</h1>
-        <p className="font-work text-lg text-gray-500">
+        <h1 className="font-work text-5xl font-extrabold text-center">
+          DaalBeat
+        </h1>
+        <p className="font-work text-lg text-gray-500 text-center">
           Healthcare Pricing Transparency
         </p>
       </main>
