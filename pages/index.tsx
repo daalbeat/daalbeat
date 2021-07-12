b4c4c2ded3505714c329a51111b42d71dbee6998
Anthony Lee <antwonlee@gmail.com>
2021-07-11 21:17:59 -0500

    Fix syntax errors

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,32 +4,32 @@ import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col justify-center p-0 m-0 overflow-visible">
-      <h1 className="font-work text-5xl font-extrabold text-center">
-        DaalBeat
-      </h1>
-      <p className="font-work text-lg text-gray-500 text-center">
-        <span className="mr-3">Healthcare pricing</span>
-        <RoughNotationGroup>
-          <RoughNotation
-            type="underline"
-            show={true}
-            color="#1e003d"
-            strokeWidth={3}
-          >
-            transparent
-          </RoughNotation>
-          <span className="mx-1">and</span>
-          <RoughNotation
-            type="underline"
-            show={true}
-            color="#1e003d"
-            strokeWidth={3}
-          >
-            simplified
-          </RoughNotation>
-        </RoughNotationGroup>
-      </p>
+    <div className="flex flex-col justify-center min-h-screen">
+      <div className="text-center mx-auto">
+        <span className="font-work text-5xl font-extrabold">DaalBeat</span>
+        <p className="font-work text-lg text-gray-500 text-center mt-3">
+          <span className="mr-2">Healthcare pricing</span>
+          <RoughNotationGroup show={true}>
+            <RoughNotation
+              type="underline"
+              color="#1e003d"
+              strokeWidth={3}
+              order={1}
+            >
+              transparent
+            </RoughNotation>
+            <span className="mx-2">and</span>
+            <RoughNotation
+              type="underline"
+              color="#1e003d"
+              strokeWidth={3}
+              order={2}
+            >
+              simplified
+            </RoughNotation>
+          </RoughNotationGroup>
+        </p>
+      </div>
     </div>
   );
 }
