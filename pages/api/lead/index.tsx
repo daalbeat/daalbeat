44cccb0959e45acecb0e23971fe8b1705a6ffd47
Anthony Lee <antwonlee@gmail.com>
2021-07-15 14:14:28 -0500

    Remove code duplication

--- pages/api/lead/index.tsx
+++ pages/api/lead/index.tsx
@@ -1,6 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../lib/prisma";
-import { PrismaClient } from "@prisma/client";
 
 export default async function handle(
   req: NextApiRequest,
