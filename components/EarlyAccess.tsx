8a600ab604ba7acb8b4cccf6db92b7d0601b0f28
Anthony Lee <antwonlee@gmail.com>
2021-07-14 23:59:40 -0500

    Add API route for LEAD

--- components/EarlyAccess.tsx
+++ components/EarlyAccess.tsx
@@ -1,24 +1,48 @@
 import React from "react";
 
 const EarlyAccess = () => {
+  const submitLead = async (event) => {
+    event.preventDefault();
+
+    const res = await fetch("/api/lead", {
+      body: JSON.stringify({
+        email: event.target.email.value,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    });
+
+    const result = await res.json();
+  };
+
   return (
     <section className="mt-20 text-center flex flex-row justify-center m-5">
-      <div className="bg-gray-50 p-10 rounded border border-gray-100 shadow-sm">
+      <div className="bg-gray-100 p-10 rounded border border-gray-200 shadow-sm">
         <span className="font-extrabold text-xl text-gradient bg-gradient-to-r from-pink-400 via-purple-400 to-indigo-500 font-work">
-          Early Access
+          Waiting List
         </span>
         <p className="text-gray-500">
-          DaalBeat is a marketplace that makes healthcare pricing transparent
-          and simplified.
+          You’ll be one of the first to get early access.
         </p>
-        <div className="mt-10">
-          <input
-            type="text"
-            name="early-access-email"
-            id="early-access-email"
-            className="flex-1 border border-gray-300 rounded-none rounded-l-md"
-            placeholder="your@email.com"
-          />
+        <div id="lead-form-card" className="mt-10">
+          <form onSubmit={submitLead}>
+            <label htmlFor="email"></label>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              className="flex-1 rounded-none rounded-l-md py-2 px-3 focus:outline-none text-sm"
+              placeholder="your@email.com"
+            />
+            <button
+              className="bg-blue-100 hover:bg-blue-200 px-3 py-2 transition duration-500 ease-in-out rounded-none rounded-r-md focus:ring font-extrabold uppercase text-sm"
+              type="submit"
+            >
+              Submit
+            </button>
+          </form>
         </div>
       </div>
     </section>
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center min-h-screen m-1">
       <div className="text-center mx-auto">
-        <span className="text-gradient bg-gradient-to-r from-pink-400 via-purple-400 to-indigo-500 font-work text-5xl font-extrabold">
-          DaalBeat
-        </span>
+        <span className="font-work text-4xl font-extrabold">DAALBEAT</span>
         <p className="font-work text-lg text-gray-500 text-center mt-3">
           <span className="mr-2">Healthcare pricing</span>
           <RoughNotationGroup show={true}>
