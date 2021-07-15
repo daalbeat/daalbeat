de863a1aea2df2bdd4602797b9971647c1337e41
Anthony Lee <antwonlee@gmail.com>
2021-07-14 22:17:27 -0500

    Update description

--- components/EarlyAccess.tsx
+++ components/EarlyAccess.tsx
@@ -4,18 +4,21 @@ const EarlyAccess = () => {
   return (
     <section className="mt-20 text-center flex flex-row justify-center m-5">
       <div className="bg-gray-50 p-10 rounded border border-gray-100 shadow-sm">
-        <span className="font-extrabold text-xl">Early Access</span>
-        <p>Hello world.</p>
-        <div className="grid grid-cols-2">
-          <div>
-            <input
-              type="text"
-              name="early-access-email"
-              id="early-access-email"
-              className="flex-1 border border-gray-300 rounded-none rounded-l-md"
-              placeholder="your@email.com"
-            />
-          </div>
+        <span className="font-extrabold text-xl text-gradient bg-gradient-to-r from-pink-400 via-purple-400 to-indigo-500 font-work">
+          Early Access
+        </span>
+        <p className="text-gray-500">
+          DaalBeat is a marketplace that makes healthcare pricing transparent
+          and simplified.
+        </p>
+        <div className="mt-10">
+          <input
+            type="text"
+            name="early-access-email"
+            id="early-access-email"
+            className="flex-1 border border-gray-300 rounded-none rounded-l-md"
+            placeholder="your@email.com"
+          />
         </div>
       </div>
     </section>
