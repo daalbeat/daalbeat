50625239cfda310bd1da3e4cfcbb2cdb524ffffe
Anthony Lee <antwonlee@gmail.com>
2021-07-16 16:33:55 -0500

    feat: Add basic UI (#6)

---
+++ components/ui/Columns.tsx
@@ -0,0 +1,122 @@
+const Columns = () => {
+  return (
+    <>
+      <div className="container mx-auto mt-10 text-4xl font-extrabold">
+        Grid
+      </div>
+      <div className="container mx-auto border p-10 mt-10 rounded-lg">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Enim quo esse,
+        cupiditate obcaecati dolores doloribus officiis beatae eos sunt optio
+        mollitia accusantium nemo facere iure quod inventore incidunt itaque!
+        Sunt!
+      </div>
+
+      <div className="container mx-auto flex flex-row mt-10">
+        <div className="bg-gray-100 p-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+        <div className="bg-gray-200 p-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+      </div>
+
+      <div className="container mx-auto flex flex-row mt-10">
+        <div className="bg-gray-100 p-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+        <div className="bg-gray-200 p-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+        <div className="bg-gray-300 p-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+      </div>
+
+      <div className="container mx-auto flex flex-row mt-10">
+        <div className="bg-gray-100 p-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+        <div className="bg-gray-200 p-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+        <div className="bg-gray-300 p-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+        <div className="bg-gray-400 p-10">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+      </div>
+
+      <div className="container mx-auto flex flex-row justify-center mt-10">
+        <div className="bg-gray-100 p-10 w-80">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+        <div className="bg-gray-200 p-10 w-80">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+        <div className="bg-gray-300 p-10 w-80">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+      </div>
+
+      <div className="container mx-auto flex flex-row justify-center mt-10 space-x-6">
+        <div className="bg-gray-100 p-10 w-80">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+        <div className="bg-gray-200 p-10 w-80">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+        <div className="bg-gray-300 p-10 w-80">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
+          saepe nisi magnam eos nihil praesentium incidunt eius inventore esse
+          vel, reiciendis adipisci impedit fugiat, nam perspiciatis dolores eum!
+          Distinctio, maxime.
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Columns;
