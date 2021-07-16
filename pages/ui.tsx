fdfa411d00a590de081e0421e89b7293d1f38601
Anthony Lee <antwonlee@gmail.com>
2021-07-16 15:20:11 -0500

    Add basic UI

---
+++ pages/ui.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Columns from "../components/ui/Columns";
+import Cards from "../components/ui/Cards";
+
+const UI = () => {
+  return (
+    <>
+      <div className="p-10">
+        <Columns />
+        <Cards />
+      </div>
+    </>
+  );
+};
+
+export default UI;
