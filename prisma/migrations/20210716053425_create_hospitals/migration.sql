dc1f5269dbd59a7019b877224ea18356495ad2c3
Anthony Lee <antwonlee@gmail.com>
2021-07-17 20:28:55 -0500

    fix: Fix migration file manually: hospital state field

--- prisma/migrations/20210716053425_create_hospitals/migration.sql
+++ prisma/migrations/20210716053425_create_hospitals/migration.sql
@@ -24,6 +24,7 @@ CREATE TABLE "hospitals" (
     "facilityName" TEXT NOT NULL,
     "address" TEXT NOT NULL,
     "city" TEXT NOT NULL,
+    "state" TEXT NOT NULL,
     "zipcode" TEXT NOT NULL,
     "countyName" TEXT NOT NULL,
     "phoneNumber" TEXT NOT NULL,
