ba7ec8dc720d13f86336764df4a6705f6637a27a
Anthony Lee <antwonlee@gmail.com>
2021-07-19 00:17:50 -0500

    Add rating stars

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,26 +9,14 @@ export default function Home() {
       <div className="text-center mx-auto">
         <span className="font-work text-4xl font-extrabold">DAALBEAT</span>
         <p className="font-work text-lg text-gray-500 text-center mt-3">
-          <span className="mr-2">Healthcare marketplace</span>
-          <RoughNotationGroup show={true}>
-            <RoughNotation
-              type="underline"
-              color="#f073ba"
-              strokeWidth={3}
-              order={1}
-            >
-              transparent
-            </RoughNotation>
-            <span className="mx-2">and</span>
-            <RoughNotation
-              type="underline"
-              color="#746ef3"
-              strokeWidth={3}
-              order={2}
-            >
-              simplified
-            </RoughNotation>
-          </RoughNotationGroup>
+          <RoughNotation
+            type="underline"
+            color="#f073ba"
+            strokeWidth={3}
+            show={true}
+          >
+            Price transparency healthcare marketplace.
+          </RoughNotation>
         </p>
       </div>
 
