f451ee3c71da2e36b445dcf5f68d6e2db84a5fcd
Anthony Lee <antwonlee@gmail.com>
2021-07-20 00:05:53 -0500

    Add daalbeat logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,16 @@
 import Head from "next/head";
+import Image from "next/image";
 import { signIn, signOut, useSession } from "next-auth/client";
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 import EarlyAccessForm from "../components/EarlyAccessForm";
+import DaalBeatLogo from "../public/DaalBeatLogo.png";
 
 export default function Home() {
   return (
     <div className="flex flex-col justify-center min-h-screen m-1">
       <div className="text-center mx-auto">
-        <span className="font-work text-4xl font-extrabold">DAALBEAT</span>
-        <p className="font-work text-lg text-gray-500 text-center mt-3">
+        <Image src={DaalBeatLogo} alt="DaalBeat Logo" width={300} height={53} />
+        <p className="font-work text-lg text-gray-500 text-center">
           <RoughNotation
             type="underline"
             color="#f073ba"
