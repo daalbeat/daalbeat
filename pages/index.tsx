bcd3eb8300d3c886275f463f521abdf09db0c5c2
Anthony Lee <antwonlee@gmail.com>
2022-05-24 21:25:40 -0500

    feat: Add :rainbow:

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+// NextJS
 import Head from "next/head";
 import Image from "next/image";
 import { signIn, signOut, useSession } from "next-auth/client";
+
+// React
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+// DaalBeat
 import EarlyAccessForm from "../components/EarlyAccessForm";
 import DaalBeatLogo from "../public/DaalBeatLogo.png";
 import Services from "../components/Services";
@@ -9,9 +16,9 @@ import Services from "../components/Services";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center min-h-screen m-1">
-      <div className="text-center mx-auto mt-10 md:mt-0">
+      <div className="mx-auto mt-10 text-center md:mt-0">
         <Image src={DaalBeatLogo} alt="DaalBeat Logo" width={300} height={53} />
-        <p className="font-work text-lg text-gray-500 text-center">
+        <p className="text-lg text-center text-gray-500 font-work">
           <RoughNotation
             type="underline"
             color="#450069"
@@ -23,6 +30,9 @@ export default function Home() {
         </p>
       </div>
       <Services />
+      <div className="mx-auto mt-10 text-center">
+        <ConnectButton />
+      </div>
       <EarlyAccessForm />
     </div>
   );
