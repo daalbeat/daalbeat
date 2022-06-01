79e78db403368bca85afb87788cddec77d33a0e1
Anthony Lee <antwonlee@gmail.com>
2022-06-01 14:01:01 -0500

    chore: Update rainbow wallet

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,18 +8,13 @@ import type { AppProps } from "next/app";
 
 // React
 import "@rainbow-me/rainbowkit/styles.css";
-
-import {
-  apiProvider,
-  configureChains,
-  getDefaultWallets,
-  RainbowKitProvider,
-} from "@rainbow-me/rainbowkit";
-import { chain, createClient, WagmiProvider } from "wagmi";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { configureChains, chain, createClient, WagmiConfig } from "wagmi";
 
 const { chains, provider } = configureChains(
   [chain.mainnet, chain.polygon, chain.optimism, chain.arbitrum],
-  [apiProvider.alchemy(process.env.ALCHEMY_ID), apiProvider.fallback()]
+  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID })]
 );
 
 const { connectors } = getDefaultWallets({
@@ -35,13 +30,13 @@ const wagmiClient = createClient({
 
 function DaalBeat({ Component, pageProps }: AppProps) {
   return (
-    <WagmiProvider client={wagmiClient}>
+    <WagmiConfig client={wagmiClient}>
       <RainbowKitProvider chains={chains}>
         <Layout>
           <Component {...pageProps} />
         </Layout>
       </RainbowKitProvider>
-    </WagmiProvider>
+    </WagmiConfig>
   );
 }
 export default DaalBeat;
