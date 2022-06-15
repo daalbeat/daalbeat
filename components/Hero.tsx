06a4f2e1818247a3023f007d22912138efbd9de4
Anthony Lee <antwonlee@gmail.com>
2022-06-15 03:44:29 -0500

    feat: Add landing page (#24)

---
+++ components/Hero.tsx
@@ -0,0 +1,35 @@
+export default function Hero() {
+  return (
+    <>
+      <main className="px-4 mx-auto mt-16 max-w-7xl sm:mt-40">
+        <div className="text-center">
+          <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+            <span className="block xl:inline">Healthcare Toolkit on the</span>{" "}
+            <span className="block text-purple-900 xl:inline">Blockchain</span>
+          </h1>
+          <p className="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
+            New kind of healthcare emergency fund and pricing transparency.
+          </p>
+          <div className="max-w-md mx-auto mt-5 sm:flex sm:justify-center md:mt-8">
+            <div className="rounded-md shadow">
+              <a
+                href="https://docs.daalbeat.com"
+                className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-purple-900 border border-transparent rounded-md hover:bg-purple-700 md:py-4 md:text-lg md:px-10"
+              >
+                Get started
+              </a>
+            </div>
+            <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+              <a
+                href="https://discord.gg/wgv22BAQDu"
+                className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-purple-900 bg-white border border-transparent rounded-md hover:bg-gray-50 md:py-4 md:text-lg md:px-10"
+              >
+                Join Community
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,128 @@
+// NextJS
+import Image from "next/image";
+
+// ReactJS
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+
+// DaalBeat
+import DaalBeatLogo from "../public/DaalBeatLogo.png";
+
+const navigation = [
+  { name: "Docs", href: "https://docs.daalbeat.com" },
+  {
+    name: "White Paper",
+    href: "https://docs.google.com/document/d/1mDc95O6c_-btTEsCr1hs1W5-eMEHwI9Pb9eaI-y0BKs/edit?usp=sharing",
+  },
+  { name: "Discord", href: "https://discord.gg/wgv22BAQDu" },
+];
+
+export default function Navbar() {
+  return (
+    <div className="relative overflow-hidden ">
+      <div className="relative pt-6">
+        <Popover>
+          <div className="px-4 pb-6 mx-auto max-w-7xl sm:px-10">
+            <nav
+              className="relative flex items-center justify-between sm:h-10 md:justify-center"
+              aria-label="Global"
+            >
+              <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
+                <div className="flex items-center justify-between w-full md:w-auto">
+                  <a href="/">
+                    <span className="sr-only">Workflow</span>
+                    <Image
+                      src={DaalBeatLogo}
+                      className="w-auto h-8 sm:h-10"
+                      alt="DaalBeat"
+                      width={200}
+                      height={35}
+                    />
+                  </a>
+                  <div className="flex items-center -mr-2 md:hidden">
+                    <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 rounded-md bg-gray-50 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-purple-500">
+                      <span className="sr-only">Open main menu</span>
+                      <MenuIcon className="w-6 h-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+              </div>
+              <div className="hidden md:flex md:space-x-10">
+                {navigation.map((item) => (
+                  <a
+                    key={item.name}
+                    href={item.href}
+                    className="font-medium text-gray-500 hover:text-gray-900"
+                  >
+                    {item.name}
+                  </a>
+                ))}
+              </div>
+              <div className="hidden md:absolute md:flex md:items-center md:justify-end md:inset-y-0 md:right-0">
+                <span className="inline-flex rounded-md shadow">
+                  <a
+                    href="#"
+                    className="inline-flex items-center px-4 py-2 text-base font-medium text-purple-900 bg-white border border-gray-200 rounded-md hover:bg-gray-50"
+                  >
+                    Launch App (Soon)
+                  </a>
+                </span>
+              </div>
+            </nav>
+          </div>
+
+          <Transition
+            as={Fragment}
+            enter="duration-150 ease-out"
+            enterFrom="opacity-0 scale-95"
+            enterTo="opacity-100 scale-100"
+            leave="duration-100 ease-in"
+            leaveFrom="opacity-100 scale-100"
+            leaveTo="opacity-0 scale-95"
+          >
+            <Popover.Panel
+              focus
+              className="absolute inset-x-0 top-0 z-10 p-2 transition origin-top-right transform md:hidden"
+            >
+              <div className="overflow-hidden bg-white rounded-lg shadow-md ring-1 ring-black ring-opacity-5">
+                <div className="flex items-center justify-between px-5 pt-4">
+                  <div>
+                    <img
+                      className="w-auto h-8"
+                      src="https://tailwindui.com/img/logos/workflow-mark-purple-900.svg"
+                      alt=""
+                    />
+                  </div>
+                  <div className="-mr-2">
+                    <Popover.Button className="inline-flex items-center justify-center p-2 text-gray-400 bg-white rounded-md hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-purple-500">
+                      <span className="sr-only">Close menu</span>
+                      <XIcon className="w-6 h-6" aria-hidden="true" />
+                    </Popover.Button>
+                  </div>
+                </div>
+                <div className="px-2 pt-2 pb-3">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="block px-3 py-2 text-base font-medium text-gray-700 rounded-md hover:text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <a
+                  href="#"
+                  className="block w-full px-5 py-3 font-medium text-center text-purple-900 bg-gray-50 hover:bg-gray-100"
+                >
+                  Launch App (Soon)
+                </a>
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </Popover>
+      </div>
+    </div>
+  );
+}
