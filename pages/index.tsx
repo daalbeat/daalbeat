6163401aad48cdfe8b90352c58f6d7697fa1eb8b
Anthony Lee <antwonlee@gmail.com>
2022-06-18 10:27:37 -0500

    fix: Fix navbar phone

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,14 +6,12 @@ import { useAccount } from "wagmi";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 // DaalBeat
-import Hero from "../components/Hero";
 import Team from "../components/Team";
 import Newsletter from "../components/Newsletter";
 
 export default function Home() {
   return (
     <>
-      <Hero />
       <Newsletter />
       <Team />
     </>
