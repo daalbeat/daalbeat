60ac9fe055bcd93b3f3d76d764d92d6e9ecd21fe
Anthony Lee <antwonlee@gmail.com>
2022-06-18 10:58:47 -0500

    feat: Add features (#28)

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,11 +7,13 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 // DaalBeat
 import Team from "../components/Team";
+import Features from "../components/Features";
 import Newsletter from "../components/Newsletter";
 
 export default function Home() {
   return (
     <>
+      <Features />
       <Newsletter />
       <Team />
     </>
