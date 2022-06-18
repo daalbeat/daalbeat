de6523c678306f185bd711e863b8e4e8ba45d9da
Anthony Lee <antwonlee@gmail.com>
2022-06-18 10:33:08 -0500

    chore: Update primary cta to join waitlist

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,10 +21,10 @@ export default function Hero() {
           <div className="max-w-md mx-auto mt-5 sm:flex sm:justify-center md:mt-8">
             <div className="rounded-md shadow">
               <a
-                href="https://docs.daalbeat.com"
+                href="#join-waitlist"
                 className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-purple-900 border border-transparent rounded-md hover:bg-purple-700 md:py-4 md:text-lg md:px-10"
               >
-                Get started
+                Join Waitlist
               </a>
             </div>
             <div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
