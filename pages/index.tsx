814c6db9b7c82712888d478f9d962e457b829c51
Anthony Lee <antwonlee@gmail.com>
2022-06-18 16:05:27 -0500

    style: Add hero banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,13 +6,15 @@ import { useAccount } from "wagmi";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 // DaalBeat
-import Team from "../components/Team";
+import Banner from "../components/Banner";
 import Features from "../components/Features";
 import Newsletter from "../components/Newsletter";
+import Team from "../components/Team";
 
 export default function Home() {
   return (
     <>
+      <Banner />
       <Features />
       <Newsletter />
       <Team />
