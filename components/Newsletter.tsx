e10dc2828ca97193006cf9df3cff72bba728daf3
Anthony Lee <antwonlee@gmail.com>
2022-06-19 01:10:45 -0500

    style: Add animated button (#31)

--- components/Newsletter.tsx
+++ components/Newsletter.tsx
@@ -1,12 +1,11 @@
 // ReactJS
 import { useForm } from "react-hook-form";
-import { motion } from "framer-motion";
 
 export default function Newsletter() {
   const {
     register,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isSubmitting },
     reset,
   } = useForm();
 
@@ -138,9 +137,35 @@ export default function Newsletter() {
                 <div className="mt-4 sm:mt-0 sm:ml-3">
                   <button
                     type="submit"
+                    disabled={isSubmitting}
                     className="block w-full px-5 py-3 text-base font-medium text-white bg-indigo-500 border border-transparent rounded-md shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-purple-900 sm:px-10"
                   >
-                    Join Waitlist
+                    {isSubmitting && (
+                      <div className="flex">
+                        <svg
+                          className="w-5 h-5 mr-3 -ml-1 text-white animate-spin"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <circle
+                            className="opacity-25"
+                            cx="12"
+                            cy="12"
+                            r="10"
+                            stroke="currentColor"
+                            strokeWidth="4"
+                          ></circle>
+                          <path
+                            className="opacity-75"
+                            fill="currentColor"
+                            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                          ></path>
+                        </svg>
+                        <div>Submitting</div>
+                      </div>
+                    )}
+                    {!isSubmitting && <span>Submit</span>}
                   </button>
                 </div>
               </form>
