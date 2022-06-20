0d2b7fa39f779235ae138cbade245419ce3a2a7f
Anthony Lee <antwonlee@gmail.com>
2022-06-20 15:47:23 -0500

    chore: Add launch app button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -33,7 +33,7 @@ export default function Navbar() {
               <div className="flex items-center flex-1 md:absolute md:inset-y-0 md:left-0">
                 <div className="flex items-center justify-between w-full md:w-auto">
                   <a href="/">
-                    <span className="sr-only">Workflow</span>
+                    <span className="sr-only">DaalBeat Labs</span>
                     <Image
                       src={DaalBeatLogo}
                       className="w-auto h-8 sm:h-10"
