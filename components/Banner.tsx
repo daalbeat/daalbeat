89ccc19ba1209a55b00043c05712feb26d6647c3
Anthony Lee <antwonlee@gmail.com>
2022-06-27 21:40:27 -0500

    fix: Banner width

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -4,8 +4,8 @@ import Hero from "../public/hero.jpg";
 
 export default function HeroImage() {
   return (
-    <div className="px-4 mt-20 lg:mt-40 lg:px-20">
-      <div className="mx-auto rounded-lg md:container">
+    <div className="px-4 mt-20 lg:mt-40 lg:px-20 ">
+      <div className="px-4 pb-6 mx-auto max-w-7xl sm:px-10">
         <Image
           src={Hero}
           className="rounded-lg"
