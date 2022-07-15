4f7692a0f7dff9c80867f87fc5971b141fd67cae
Anthony Lee <antwonlee@gmail.com>
2022-07-15 10:16:17 -0500

    chore: Update main tagline

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,11 +11,12 @@ export default function Hero() {
           viewport={{ once: true, amount: 0.8 }}
           className="text-center"
         >
-          <h1 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-3xl md:text-4xl">
+          <h1 className="text-2xl font-bold tracking-tight text-gray-900 sm:text-3xl md:text-3xl">
             <span className="block xl:inline">New Kind of </span>
             <span className="block text-purple-900 xl:inline">
               Healthcare Emergency Funding
             </span>
+            <span className="block xl:inline"> on Blockchain.</span>
           </h1>
           <p className="max-w-md mx-auto mt-3 text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
             Save and protect with friends and family.
